8207348162bf4d33c17f94c31ca1e3bcfbebe13e
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-09 11:32:30 +0600

    city   🤡

---
+++ city.py
@@ -0,0 +1,8 @@
+# Prints out the name of a favorite city
+def favorite_city(name):
+    print("One of my favorite cities is", name)
+
+favorite_city("Berlin, Germany")
+favorite_city("Newyork,America ")
+favorite_city("Tokio, Japan")
+# Author : Hazrat Ali
